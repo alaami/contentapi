c6710fc0b130e943d2ca024615b55c7e702e1ed4
Alaa <mir.alaa96@gmail.com>
2020-04-15 02:06:51 -0400

    1 st deploy add fix 12

--- api/articles.js
+++ api/articles.js
@@ -3,7 +3,7 @@ const url = require('url')
 
 //Connect to mongoDB
 var mongoose   = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true });
+await mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true });
 module.exports = async (req, res) => {
 	        Article.find(function(err, articles) {
             if (err)
